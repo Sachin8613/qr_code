1f3e62a3293851eeaae4783e0c9ef46763f6312b
Sachin8613 <sachinamrule@gmail.com>
2020-02-09 09:58:07 +0530

    QRCode created

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _displayQRCode("Flutter");
+        },
+        tooltip: 'QR Code',
+        child: Icon(Icons.keyboard_backspace),
+      ),
+    );
+  }
+  Future _displayQRCode(String data){
+    return showDialog(
+        context: context,
+        builder: (context) {
+
+          return Container(
+            child: AlertDialog(
+              title: Text('Your QR COde : '),
+              content: Container(
+                height: 300,
+                width: 300,
+                child: QrImage(
+                  data: data,
+                  version: QrVersions.auto,
+                  gapless: false,
+                  size: 200,
+                ),
+              ),
+                actions: <Widget>[
+                  new FlatButton(
+                    child: new Text('Ok'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  new FlatButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ]
+
+            ),
+          );
+
+        });
+  }
+}
